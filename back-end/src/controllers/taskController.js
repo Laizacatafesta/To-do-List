aa8d6bbf948f1be9e7d424ec9ee51767480b57ba
Laiza Catafesta <72575967+Laizacatafesta@users.noreply.github.com>
2025-06-03 11:24:36 +0200

    did create task

--- back-end/src/controllers/taskController.js
+++ back-end/src/controllers/taskController.js
@@ -11,6 +11,15 @@ class TaskController {
             res.status(500).json({message: `${erro.message} - Falha na requisição`});
         };
     };
+
+    static async criartTask(req, res) {
+        try {
+            const taskCriada = await task.create(req.body);
+            res.status(200).json({message: "Taks criada com sucesso", task: taskCriada});
+        } catch (erro) {
+            res.status(500).json({message: `${erro.message} - Falha ao criar task`});
+        };
+    };
 };
 
 export default TaskController;
