6346f08f746dda6de3412a932622ab9831aff486
Laiza Catafesta <72575967+Laizacatafesta@users.noreply.github.com>
2025-06-03 11:42:58 +0200

    did delete task

--- back-end/src/controllers/taskController.js
+++ back-end/src/controllers/taskController.js
@@ -30,6 +30,16 @@ class TaskController {
             res.status(500).json({message: `${erro.message} - Falha na atualização da task.`});
         };
     };
+
+    static async deleteTask(req, res) {
+        try {
+            const id = req.params.id;
+            const taksdeletada = await task.findByIdAndDelete(id);
+            res.status(200).json({message: "Livro removido com sucesso."});
+        } catch (erro) {
+            res.status(500).json({message: `${erro.message} - Falha ao remover livro`});
+        };
+    };
 };
 
 export default TaskController;
