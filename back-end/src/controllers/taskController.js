30e9b33cfca923ead714ddc88862855d3e67b4a6
Laiza Catafesta <72575967+Laizacatafesta@users.noreply.github.com>
2025-06-03 11:34:47 +0200

    did task updated

--- back-end/src/controllers/taskController.js
+++ back-end/src/controllers/taskController.js
@@ -20,6 +20,16 @@ class TaskController {
             res.status(500).json({message: `${erro.message} - Falha ao criar task`});
         };
     };
+
+    static async atualizarTask(req, res) {
+        try {
+            const id = req.params.id;
+            const taskAtualizada = await task.findByIdAndUpdate(id, req.body);
+            res.status(200).json({message: "Task atualizada com sucesso"});
+        } catch (erro) {
+            res.status(500).json({message: `${erro.message} - Falha na atualização da task.`});
+        };
+    };
 };
 
 export default TaskController;
