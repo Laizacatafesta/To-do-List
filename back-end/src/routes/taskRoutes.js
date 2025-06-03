371e384d8b81762219320c7f0d4cc3920c857563
Laiza Catafesta <72575967+Laizacatafesta@users.noreply.github.com>
2025-06-03 11:25:35 +0200

    did route to create task

--- back-end/src/routes/taskRoutes.js
+++ back-end/src/routes/taskRoutes.js
@@ -4,5 +4,6 @@ import TaskController from "../controllers/taskController.js";
 const routes  = express.Router();
 
 routes.get("/task", TaskController.listarTasks);
+routes.post("/taks", TaskController.criartTask);
 
 export default routes;
