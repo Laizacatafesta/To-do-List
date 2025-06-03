edc134882a984b04a4d9ca80c80da42f30c1f313
Laiza Catafesta <72575967+Laizacatafesta@users.noreply.github.com>
2025-06-03 11:46:14 +0200

    fixed delete controller

--- back-end/src/controllers/taskController.js
+++ back-end/src/controllers/taskController.js
@@ -34,7 +34,7 @@ class TaskController {
     static async deleteTask(req, res) {
         try {
             const id = req.params.id;
-            const taksdeletada = await task.findByIdAndDelete(id);
+            await task.findByIdAndDelete(id);
             res.status(200).json({message: "Livro removido com sucesso."});
         } catch (erro) {
             res.status(500).json({message: `${erro.message} - Falha ao remover livro`});
--- back-end/src/routes/taskRoutes.js
+++ back-end/src/routes/taskRoutes.js
@@ -6,5 +6,6 @@ const routes  = express.Router();
 routes.get("/task", TaskController.listarTasks);
 routes.post("/task", TaskController.criartTask);
 routes.put("/task/:id", TaskController.atualizarTask);
+routes.delete("/task/:id", TaskController.deleteTask);
 
 export default routes;
