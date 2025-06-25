441a720e7ad2351acb1f72a3a8a662fe3d9b93e0
Laiza Catafesta <72575967+Laizacatafesta@users.noreply.github.com>
2025-06-25 10:15:49 +0200

    added map to the priority

--- front-end/src/Componentes/Prioridades/index.js
+++ front-end/src/Componentes/Prioridades/index.js
@@ -1,12 +1,17 @@
+import Task from "../Task"
 import "./Prioridades.css"
 
 const Prioridades = (props) => {
     return(
-        <section>
+        (props.task.length > 0) ? <section>
             <div>
                 <h3></h3>
+                <div>
+                    {props.task.map(task => <Task key={task.titulo} titulo={task.titulo} objetivo={task.objetivo}/>)}
+                </div>
             </div>
         </section>
+        :''
     )
 }
 
